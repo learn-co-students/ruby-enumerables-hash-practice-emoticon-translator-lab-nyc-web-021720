bfb7b5c2798a99f7f66bdb8b4dd70493175553d3
Yahya  Gok <ygok17@hotmail.com>
2020-02-10 21:23:54 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,19 +10,35 @@ def load_library(path)
   emo_hash = {"get_meaning" => {}, "get_emoticon" => {}}
   
   emoticons.each do | emotion,array|
-    emo_hash['get_meaning'] 
-    
+    # binding.pry
+     emo_hash['get_meaning'][array[1]] = emotion
+     emo_hash['get_emoticon'][array[0]] = array[1]
+  end 
+  emo_hash 
   # code goes here
 end
 
-def get_japanese_emoticon
-  load_library
-  # code goes here
+def get_japanese_emoticon(path, emoticon )
+  emoticons =  load_library(path)
+ # binding.pry 
+  emoticon_japanese = emoticons['get_emoticon'][emoticon]
+  if emoticons["get_emoticon"].include?(emoticon) 
+    emoticons["get_emoticon"][emoticon]
+  else 
+    "Sorry, that emoticon was not found"
+  
+end
+  
 end
 
-def get_english_meaning
-  load_library
-  # code goes her
+def get_english_meaning(path, emoticon)
+   emoticons = load_library(path)
+  
+  if emoticons['get_meaning'].include?(emoticon)
+    emoticons["get_meaning"][emoticon]
+  else 
+     "Sorry, that emoticon was not found"
+   end
 end
 
 
