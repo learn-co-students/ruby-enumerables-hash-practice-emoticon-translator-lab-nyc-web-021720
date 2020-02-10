802f060353c0a0de86f0c093d5cc28b8fb969b02
Yahya  Gok <ygok17@hotmail.com>
2020-02-10 19:17:13 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,43 @@
+require "yaml"
+require 'pry'
+
+ 
+
 # require modules here
 
-def load_library
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  emo_hash = {"get_meaning" => {}, "get_emoticon" => {}}
+  
+  emoticons.each do | emotion,array|
+    emo_hash['get_meaning'] 
+    
   # code goes here
 end
 
 def get_japanese_emoticon
+  load_library
   # code goes here
 end
 
 def get_english_meaning
-  # code goes here
+  load_library
+  # code goes her
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
