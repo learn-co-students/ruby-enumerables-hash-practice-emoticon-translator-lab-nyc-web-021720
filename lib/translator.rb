adb418cb3f5fa59d511044e0f356a9e1c60b3c2f
Johnson Kow <jkow95@gmail.com>
2020-02-11 04:14:58 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,46 @@
-# require modules here
+require "yaml"
+require 'pp'
 
-def load_library
-  # code goes here
+def load_library(path)
+  
+ emoticons = YAML.load_file(path)
+
+  new_hash = {}
+  new_hash[:get_emoticon]= {}
+  new_hash[:get_meaning] = {}
+
+  emoticons.each do |key,array|
+    new_hash[:get_emoticon][array[0]] = array[1] #Why can't I just put "array[1]"
+    new_hash[:get_meaning][array[1]] = key
+  end
+  pp new_hash
+  
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon (path, emoticon)
+  result = load_library(path)[:get_emoticon][emoticon]
+  result ? result : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path,emoticon)
+  result = load_library(path)[:get_meaning][emoticon]
+  result ? result : "Sorry, that emoticon was not found"
 end
+
+
+
+
+
+
+ #thing = YAML.load_file(path)
+  #pp thing
+  
+  #newHash = {}
+  #newHash[:get_meaning]= {} 
+  #newHash[:get_emoticon] = {}
+  
+  #thing.each { |key,array|
+   # newHash[:get_meaning] = key
+  #  newHash[:get_emoticon][array[0]] = thing[key][1]
+  #}
+  #pp newHash
